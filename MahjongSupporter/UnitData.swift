74c940f7bf4526b0aed72254f4e20d229941236d
wiwi-git <duse34@hotmail.com>
2021-07-15 13:05:16 +0900

    패 데이터 만드는중

---
+++ MahjongSupporter/UnitData.swift
@@ -0,0 +1,20 @@
+//
+//  UnitData.swift
+//  MahjongSupporter
+//
+//  Created by Emoticbox on 2021/07/15.
+//
+
+import Foundation
+
+class Units: NSObject {
+    let text = [
+        //자패
+        "東",
+        "南",
+        "北",
+        "南",
+        "萬",//만날 만
+        "発",//쏠 발
+    ]
+}
